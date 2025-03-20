5af9048b57eea787d3f302ea761119218a514654
Vicentedelidiii <vicentedelidiii@gmail.com>
2025-03-20 15:33:54 +0800

    battleship v2 & fucntion

---
+++ Getting Functional/Function.js
@@ -0,0 +1,31 @@
+var dogName = "Lester";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says wooF wooF");
+}
+
+var dogName = "Lester";
+var dogWeight = 25;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says wooF wooF");
+}
+
+var dogName = "Lester";
+var dogWeight = 18;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says wooF wooF");
+}
+
+var dogName = "Lester";
+var dogWeight = 17;
+if (dogWeight > 20) {
+    console.log(dogName + " says WOOF WOOF");
+} else {
+    console.log(dogName + " says wooF wooF");
+}
