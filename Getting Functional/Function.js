6f5153e6300c32f3f7999dcf6180b932c42a1122
Vicentedelidiii <vicentedelidiii@gmail.com>
2025-03-24 15:19:10 +0800

    Function Activity 1

--- Getting Functional/Function.js
+++ Getting Functional/Function.js
@@ -1,3 +1,4 @@
+/*
 var dogName = "Lester";
 var dogWeight = 23;
 if (dogWeight > 20) {
@@ -29,3 +30,18 @@ if (dogWeight > 20) {
 } else {
     console.log(dogName + " says wooF wooF");
 }
+*/
+
+function bark(name,weight){
+    if(weight > 20){
+        console.log(name +"says WOOF WOOF");
+    }
+    else{
+        console.log(name +"says woof woof")
+    }
+}
+
+bark("John", 15)
+bark("Lester", 14)
+bark("Lawag", 25)
+bark("Villasoto", 29)
